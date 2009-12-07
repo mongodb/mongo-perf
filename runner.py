352f2b60fe997144a0bb360274c60e9ae7fdbd28
Mathias Stearn <redbeard0531@gmail.com>
2009-12-07 01:02:23 -0500

    Added basic runner script

---
+++ runner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python2
+
+import sys
+import subprocess
+import os
+import shutil
+
+if not os.path.exists('./tmp/mongo'):
+    subprocess.check_call(['git', 'clone', 'http://github.com/mongodb/mongo.git'], cwd='./tmp')
+
+if os.path.exists('./tmp/data/'):
+    shutil.rmtree('./tmp/data/')
+
+branch = sys.argv[1] if (len(sys.argv) > 1) else 'master'
+subprocess.check_call(['git', 'checkout', branch], cwd='./tmp/mongo')
+
+if branch == 'master':
+    subprocess.check_call(['git', 'pull'], cwd='./tmp/mongo')
+
+subprocess.check_call(['scons'], cwd='./tmp/mongo')
+
+mongod = subprocess.Popen(['./tmp/mongo/mongod', '--dbpath', './tmp/data/', '--port', '30027'])
+
+benchmark_results=''
+try:
+    benchmark = subprocess.Popen(['./benchmark'], stdout=subprocess.PIPE)
+    benchmark_results = benchmark.read()
+    benchmark.wait()
+finally:
+    mongod.terminate()
+    mongod.wait()
+
+print benchmark_results
+
+
+
+
