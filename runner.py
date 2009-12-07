22ea202f6d4ca6f1d52421b4a384d2efae3fb943
Mathias Stearn <redbeard0531@gmail.com>
2009-12-07 10:16:56 -0500

    fixes for runner.py

--- runner.py
+++ runner.py
@@ -4,12 +4,14 @@ import sys
 import subprocess
 import os
 import shutil
+import time
 
 if not os.path.exists('./tmp/mongo'):
     subprocess.check_call(['git', 'clone', 'http://github.com/mongodb/mongo.git'], cwd='./tmp')
 
 if os.path.exists('./tmp/data/'):
     shutil.rmtree('./tmp/data/')
+os.mkdir('./tmp/data/'
 
 branch = sys.argv[1] if (len(sys.argv) > 1) else 'master'
 subprocess.check_call(['git', 'checkout', branch], cwd='./tmp/mongo')
@@ -21,10 +23,12 @@ subprocess.check_call(['scons'], cwd='./tmp/mongo')
 
 mongod = subprocess.Popen(['./tmp/mongo/mongod', '--dbpath', './tmp/data/', '--port', '30027'])
 
+time.sleep(1) # wait for server to come up
+
 benchmark_results=''
 try:
     benchmark = subprocess.Popen(['./benchmark'], stdout=subprocess.PIPE)
-    benchmark_results = benchmark.read()
+    benchmark_results = benchmark.stdout.read()
     benchmark.wait()
 finally:
     mongod.terminate()
