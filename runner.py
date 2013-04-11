6d10c39953b44fb47de3a2bf7e900bb00501378a
deafgoat <deafgoat@gmail.com>
2013-04-11 17:51:24 -0400

    incorporate local.py in runner.py

--- runner.py
+++ runner.py
@@ -32,25 +32,263 @@ try:
 except ImportError:
     from pymongo.json_util import object_hook
 
-# Globals
-processes = []
-connection = None
-mongod_handle = None
-now = datetime.datetime.utcnow()
-run_date = now.strftime("%Y-%m-%d")
 
-
-def cleanup():
-    """Cleans up spawned children
+class Master(object):
     """
-    global processes
-    retval = 0
-    for p in processes:
-        terminated = p.poll()
-        if terminated is None:
-            p.kill()
-            retval = 1
-    return retval
+    """
+    def __init__(self, *args, **kwargs):
+        """ Get a definition given parameters.
+        """
+        self.opts = args[0]
+        self.connection = None
+        self.now = datetime.datetime.utcnow()
+        self.run_date = self.now.strftime("%Y-%m-%d")
+
+    def cleanup(self):
+        """Cleans up spawned children
+        """
+        retval = 0
+        for p in self.processes:
+            terminated = p.poll()
+            if terminated is None:
+                p.kill()
+                retval = 1
+        return retval
+
+
+    def prep_storage(self, opts):
+        """Gets host/build info and creates indexes
+            in the varius collections
+        """
+        build_info, host_info = None, None
+
+        if not self.opts.label:
+            self.opts.label = 'test'
+
+        try:
+            self.connection = pymongo.MongoClient(host=opts.rhost,
+                                             port=int(opts.rport))
+            raw = self.connection.bench_results.raw
+            host = self.connection.bench_results.host
+            build_info = self.connection.bench_results.command('buildInfo')
+            host_info = self.connection.bench_results.command('hostInfo')
+            info = dict({'platform': host_info,
+                         'build_info': build_info})
+            info['run_date'] = self.run_date
+            info['label'] = self.opts.label
+            raw.ensure_index('label')
+            raw.ensure_index('run_date')
+            raw.ensure_index('version')
+            raw.ensure_index('platform')
+            raw.ensure_index(
+                [('version', pymongo.ASCENDING),
+                 ('label', pymongo.ASCENDING),
+                 ('platform', pymongo.ASCENDING),
+                 ('run_date', pymongo.ASCENDING)],
+                unique=True)
+
+            host.ensure_index(
+                [('build_info.version', pymongo.ASCENDING),
+                 ('label', pymongo.ASCENDING),
+                 ('run_date', pymongo.ASCENDING)],
+                unique=True)
+
+            host.update({'build_info.version': build_info['version'],
+                         'label': self.opts.label,
+                         'run_date': self.run_date
+                         }, info, upsert=True)
+
+        except pymongo.errors.ConnectionFailure, e:
+            print >> sys.stderr, \
+                "Could not connect to MongoDB database", sys.exc_info()[0]
+            retval = self.cleanup()
+            sys.exit(retval)
+        except:
+            print >> sys.stderr, \
+                "Unexpected error in getting host/build info", sys.exc_info()[0]
+            retval = self.cleanup()
+            sys.exit(retval)
+
+        return build_info, host_info
+
+
+    def store_results(self, benchmark_results):
+        """Inserts the benchmarked object into the database
+        """
+        build_info, host_info = self.prep_storage(self.opts)
+
+        try:
+            raw = self.connection.bench_results.raw
+            benchmarks = []
+            for line in benchmark_results.split('\n'):
+                if line:
+                    obj = json.loads(line, object_hook=object_hook)
+                    benchmarks.append(obj)
+            if self.connection:
+                for benchmark in benchmarks:
+                    print benchmark
+                obj = defaultdict(dict)
+                obj['label'] = self.opts.label
+                obj['run_date'] = self.run_date
+                obj['benchmarks'] = benchmarks
+                obj['version'] = build_info['version']
+                obj['commit'] = build_info['gitVersion']
+                obj['platform'] = host_info['os']['name'].replace(" ", "_")
+                self.update_collection(raw, obj)
+        except:
+            print >> sys.stderr, "Unexpected dict error", sys.exc_info()
+            retval = self.cleanup()
+            sys.exit(retval)
+        finally:
+            if not self.opts.local:
+                self.mongod_handle.__exit__(None, None, None)
+
+
+    def update_collection(self, collection, obj):
+        """Helper to insert the benchmarked object into the database
+        """
+        try:
+            collection.update({'label': obj['label'],
+                               'version': obj['version'],
+                               'platform': obj['platform'],
+                               'run_date': obj['run_date']
+                               }, obj, upsert=True)
+        except:
+            print >> sys.stderr, "Could not update %s" % 
+                    collection, sys.exc_info()[0]
+            retval = self.cleanup()
+            sys.exit(retval)
+
+
+class Local(Master):
+    """To be run on local machine
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Local, self).__init__(*args, **kwargs)
+
+    def run_benchmark(self):
+        """Runs the benchmark tests; pulls mongod
+            from github if requested
+        """
+        mongod = None
+
+        if not self.opts.nolaunch:
+            if not os.path.exists('./tmp/mongo'):
+                subprocess.check_call(['git', 'clone',
+                                       'http://github.com/mongodb/mongo.git'], cwd='./tmp')
+            subprocess.check_call(['git', 'fetch'], cwd='./tmp/mongo')
+            subprocess.check_call(['git', 'checkout', branch], cwd='./tmp/mongo')
+
+            if os.path.exists('./tmp/data/'):
+                shutil.rmtree('./tmp/data/')
+            os.mkdir('./tmp/data/')
+
+            if branch == 'master':
+                subprocess.check_call(['git', 'pull'], cwd='./tmp/mongo')
+
+            git_info = subprocess.Popen(['git', 'log', '-1',
+                                         '--pretty=format:%H %ai'], cwd='./tmp/mongo',
+                                        stdout=subprocess.PIPE).communicate()[0]
+
+            mongodb_git, mongodb_date = git_info.split(' ', 1)
+
+            subprocess.check_call(['scons', 'mongod'], cwd='./tmp/mongo')
+
+            if self.opts.mongos:
+                mongod = subprocess.Popen(['simple-setup.py',
+                                           '--path=./tmp/mongo', '--port='+opts.port])
+                #, stdout=open(os.devnull))
+                mongodb_version += '-mongos'
+                mongodb_git += '-mongos'
+            else:
+                mongod = subprocess.Popen(['./tmp/mongo/mongod',
+                                           '--quiet', '--dbpath', './tmp/data/',
+                                           '--port', opts.port], stdout=open(os.devnull))
+
+            subprocess.check_call(['scons'], cwd='./tmp/mongo')
+
+            mongod = subprocess.Popen(['./tmp/mongo/mongod',
+                                       '--quiet', '--dbpath', './tmp/data/',
+                                       '--port', opts.port], stdout=open('/dev/null'))
+
+            print 'pid:', mongod.pid
+
+            time.sleep(1)  # wait for server to start up
+        else:
+            mongodb_git = "nolaunch"
+
+        if self.opts.label != '<git version>':
+            mongodb_git = self.opts.label
+
+        benchmark_results = ''
+        try:
+            multidb = '1' if self.opts.multidb else '0'
+            benchmark = subprocess.Popen(['./benchmark', self.opts.port,
+                                          self.opts.iterations, multidb, self.opts.username,
+                                          self.opts.password], stdout=subprocess.PIPE)
+            benchmark_results = benchmark.communicate()[0]
+            time.sleep(1)  # wait for server to clean up connections
+        finally:
+            if mongod:
+                mongod.terminate()
+                mongod.wait()
+        return benchmark_results
+
+
+class Runner(Master):
+    """To be run on buildslave
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Runner, self).__init__(*args, **kwargs)
+        self.processes = []
+        self.mongod_handle = None
+
+    def run_benchmark(self):
+        """Runs the benchmark tests
+        """
+        benchmark_results = ''
+        try:
+            multidb = '1' if self.opts.multidb else '0'
+            exe = '.exe' if os.sys.platform.startswith("win") else ''
+            mongod_path = self.opts.mongod + exe
+            self.mongod_handle = mongomgr.mongod(mongod=mongod_path,
+                                                port=self.opts.port)
+            self.mongod_handle.__enter__()
+            self.processes.append(self.mongod_handle.proc)
+        except:
+            print >> sys.stderr, "Could not start mongod", sys.exc_info()[0]
+            retval = self.cleanup()
+            sys.exit(retval)
+
+        try:
+            benchmark = subprocess.Popen(
+                ['./benchmark', self.opts.port, self.opts.iterations,
+                 multidb, self.opts.username, self.opts.password],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print >> sys.stderr, "running benchmark tests..."
+            benchmark_results = benchmark.communicate()[0]
+            time.sleep(1)  # wait for server to clean up connections
+        except:
+            print >> sys.stderr, "Could not start complete " \
+                "benchmark tests", sys.exc_info()[0]
+            retval = self.cleanup()
+            sys.exit(retval)
+        return benchmark_results
+
+
+def main():
+    opts, versions = parse_options()
+    handle = None
+    if opts.local:
+        handle = Local(opts)
+    else:
+        handle = Runner(opts)
+    # run benchmark tests
+    benchmark_results = handle.run_benchmark()
+    # store benchmark tests
+    handle.store_results(benchmark_results)
 
 
 def parse_options():
@@ -78,6 +316,9 @@ def parse_options():
     optparser.add_option('--nolaunch', dest='nolaunch',
                          help='use mongod already running on port',
                          action='store_true', default=False)
+    optparser.add_option('--local', dest='local',
+                         help='run on local machine',
+                         action='store_true', default=False)
     optparser.add_option('-m', '--multidb', dest='multidb',
                          help='use a separate db for each connection',
                          action='store_true', default=False)
@@ -91,150 +332,6 @@ def parse_options():
                          help='Password to use for authentication.',
                          type='string', default='')
     return optparser.parse_args()
-
-
-def run_benchmark(opts):
-    """Runs the benchmark tests
-    """
-    global mongod_handle, processes
-
-    benchmark_results = ''
-    try:
-        multidb = '1' if opts.multidb else '0'
-        exe = '.exe' if os.sys.platform.startswith("win") else ''
-        mongod_path = opts.mongod + exe
-        mongod_handle = mongomgr.mongod(mongod=mongod_path,
-                                        port=opts.port)
-        mongod_handle.__enter__()
-        processes.append(mongod_handle.proc)
-    except:
-        print >> sys.stderr, "Could not start mongod", sys.exc_info()[0]
-        retval = cleanup()
-        sys.exit(retval)
-
-    try:
-        benchmark = subprocess.Popen(
-            ['./benchmark', opts.port, opts.iterations,
-             multidb, opts.username, opts.password],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print >> sys.stderr, "running benchmark tests..."
-        benchmark_results = benchmark.communicate()[0]
-        time.sleep(1)  # wait for server to clean up connections
-    except:
-        print >> sys.stderr, "Could not start complete " \
-            "benchmark tests", sys.exc_info()[0]
-        retval = cleanup()
-        sys.exit(retval)
-    return benchmark_results
-
-
-def prep_storage(opts):
-    """Gets host/build info and creates indexes
-        in the varioius collections
-    """
-    global connection
-
-    build_info, host_info = None, None
-
-    try:
-        connection = pymongo.MongoClient(host=opts.rhost,
-                                         port=int(opts.rport))
-        raw = connection.bench_results.raw
-        host = connection.bench_results.host
-        build_info = connection.bench_results.command('buildInfo')
-        host_info = connection.bench_results.command('hostInfo')
-        info = dict({'platform': host_info,
-                     'build_info': build_info})
-        info['run_date'] = run_date
-        info['label'] = opts.label
-        raw.ensure_index('label')
-        raw.ensure_index('run_date')
-        raw.ensure_index('version')
-        raw.ensure_index('platform')
-        raw.ensure_index(
-            [('version', pymongo.ASCENDING),
-             ('label', pymongo.ASCENDING),
-             ('platform', pymongo.ASCENDING),
-             ('run_date', pymongo.ASCENDING)],
-            unique=True)
-
-        host.ensure_index(
-            [('build_info.version', pymongo.ASCENDING),
-             ('label', pymongo.ASCENDING),
-             ('run_date', pymongo.ASCENDING)],
-            unique=True)
-
-        host.update({'build_info.version': build_info['version'],
-                     'label': opts.label,
-                     'run_date': run_date
-                     }, info, upsert=True)
-
-    except pymongo.errors.ConnectionFailure, e:
-        print >> sys.stderr, \
-            "Could not connect to MongoDB database", sys.exc_info()[0]
-        retval = cleanup()
-        sys.exit(retval)
-    except:
-        print >> sys.stderr, \
-            "Unexpected error in getting host/build info", sys.exc_info()[0]
-        retval = cleanup()
-        sys.exit(retval)
-
-    return build_info, host_info
-
-
-def store_results(opts, benchmark_results):
-    """Inserts the benchmarked object into the database
-    """
-    global connection, mongod_handle
-
-    build_info, host_info = prep_storage(opts)
-    try:
-        raw = connection.bench_results.raw
-        benchmarks = []
-        for line in benchmark_results.split('\n'):
-            if line:
-                obj = json.loads(line, object_hook=object_hook)
-                benchmarks.append(obj)
-        if connection:
-            for benchmark in benchmarks:
-                print benchmark
-            obj = defaultdict(dict)
-            obj['label'] = opts.label
-            obj['run_date'] = run_date
-            obj['benchmarks'] = benchmarks
-            obj['version'] = build_info['version']
-            obj['commit'] = build_info['gitVersion']
-            obj['platform'] = host_info['os']['name'].replace(" ", "_")
-            update_collection(raw, obj)
-    except:
-        print >> sys.stderr, "Unexpected dict error", sys.exc_info()
-        retval = cleanup()
-        sys.exit(retval)
-    finally:
-        mongod_handle.__exit__(None, None, None)
-
-
-def update_collection(collection, obj):
-    """Helper to insert the benchmarked object into the database
-    """
-    try:
-        collection.update({'label': obj['label'],
-                           'version': obj['version'],
-                           'platform': obj['platform'],
-                           'run_date': obj['run_date']
-                           }, obj, upsert=True)
-    except:
-        print >> sys.stderr, "Could not update %s" % collection, \
-            sys.exc_info()[0]
-        retval = cleanup()
-        sys.exit(retval)
-
-
-def main():
-    opts, versions = parse_options()
-    benchmark_results = run_benchmark(opts)
-    store_results(opts, benchmark_results)
 
 if __name__ == '__main__':
     main()
