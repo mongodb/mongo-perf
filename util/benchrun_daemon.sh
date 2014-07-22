8216d27e366e1a52f9bc0f19b9b8535a7be74d9d
Quentin Conner <quentin@mongodb.com>
2014-07-22 12:51:53 -0500

    set back to original values, except for thread counts

--- util/benchrun_daemon.sh
+++ util/benchrun_daemon.sh
@@ -3,18 +3,15 @@
 #Find us if you have any questions, future user!
 set -x
 #Defaults
-MPERFPATH=${HOME}/mongo-perf
-#MPERFPATH=${HOME}/mongo-perf-qconner
-BUILD_DIR=${HOME}/mongo
-#BUILD_DIR=${HOME}/mongo-qconner
-DBPATH=${HOME}/db
+MPERFPATH=/home/mongo-perf/mongo-perf
+BUILD_DIR=/home/mongo-perf/mongo
+DBPATH=/home/mongo-perf/db
 SHELLPATH=$BUILD_DIR/mongo
 BRANCH=master
 NUM_CPUS=$(grep ^processor /proc/cpuinfo | wc -l)
 RHOST="mongo-perf-1.vpc1.build.10gen.cc"
-#RHOST="ec2-54-200-68-243.us-west-2.compute.amazonaws.com"
 RPORT=27017
-BREAK_PATH=${HOME}/build-perf
+BREAK_PATH=/home/mongo-perf/build-perf
 TEST_DIR=$MPERFPATH/testcases
 SLEEPTIME=60
 THIS_PLATFORM=`uname -s || echo unknown`
@@ -52,23 +49,23 @@ function run_mongo-perf() {
     ./mongod --dbpath "${DBPATH}" --smallfiles --fork --logpath mongoperf.log
     MONGOD_PID=$!
 
-    sleep 5
+    sleep 30
 
     cd $MPERFPATH
-    TIME="$(date "+%m%d%Y-%H:%M")"
+    TIME="$(date "+%m%d%Y|%H:%M")"
 
     TESTCASES=$(find testcases/ -name *.js)
 
     # Run with one DB.
     sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
-    python benchrun.py -l "${TIME}_${THIS_PLATFORM}" --rhost "$RHOST" --rport "$RPORT" -t 32 30 28 26 24 22 20 18 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 -s "$SHELLPATH" -f $TESTCASES
+    python benchrun.py -l "${TIME}_${THIS_PLATFORM}" --rhost "$RHOST" --rport "$RPORT" -t 16 14 12 10 8 7 6 5 4 3 2 1 -s "$SHELLPATH" -f $TESTCASES
 
     # Run with multi-DB (4 DBs.)
     sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
-    python benchrun.py -l "${TIME}_${THIS_PLATFORM}-multi" --rhost "$RHOST" --rport "$RPORT" -t 32 30 28 26 24 22 20 18 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 -s "$SHELLPATH" -m 4 -f $TESTCASES
+    python benchrun.py -l "${TIME}_${THIS_PLATFORM}-multi" --rhost "$RHOST" --rport "$RPORT" -t 16 14 12 10 8 7 6 5 4 3 2 1 -s "$SHELLPATH" -m 4 -f $TESTCASES
 
     # Kill the mongod process and perform cleanup.
-    kill -n 9 $MONGOD_PID   # doesn't get set
+    kill -n 9 ${MONGOD_PID}   # doesn't get set?
     pkill -9 mongod         # kills all mongod processes -- assumes no other use for host
     rm -rf $DBPATH/*
 
