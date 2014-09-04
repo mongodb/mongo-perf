5c175f32092f8559edc5d2c69a05f1802f76d3a4
Brian Towles <brian.towles@mongodb.com>
2014-09-04 16:56:22 -0500

    CAP-1222 - cleanup of class and documentation. moved download path to be an externally passed option to init

--- util/binarydownloader.py
+++ util/binarydownloader.py
@@ -9,10 +9,20 @@ from shutil import move
 
 
 class BinaryDownloader:
+    """ BinaryDownloader class to get latest binaries for MongoDB
 
-    # ostype can be win32, linux, osx
-    def __init__(self, ostype):
-        self.builddir = os.environ.get('BUILD_DIR', os.path.dirname(os.path.realpath(__file__)))
+    This class is a helper to download the latest binaries for mongodb based on the operating system.
+    It currently only gets the x86_64 binaries and will only get the linux/mongodb-linux-x86_64-latest.tgz type files
+
+    """
+    def __init__(self, ostype, download_dir):
+        """
+        Inits the BinaryDownloader with the Operating system type to get the MongoDB binaries for.
+        :param ostype: The os type to get,  can be win32, linux, osx, or sunos5
+        :param download_dir: the path to place the downloaded binaries tar file as well as store cached checked files
+        :return:
+        """
+        self.download_path = download_dir
         self.ostype = ostype
 
         # download and last_download is a dict with the following structure
@@ -20,20 +30,24 @@ class BinaryDownloader:
         self.download = None
         self.last_download = None
         # check file possibly holding the last_download info serialized
-        self.checkfile = os.path.join(self.builddir, '.last_download_' + ostype)
+        self.checkfile = os.path.join(self.download_path, '.last_download_' + ostype)
 
 
     def __download_file(self):
+        """
+        Private method to actually do the download of the file populated in self.download
+        :return: the final path of the downloaded file
+        """
         if self.download is None:
             raise Exception('unable to get url from site')
         # Open the url
         try:
             f = urlopen(self.download['link'])
             # Open our local file for writing
-            tmpfile = tempfile.mktemp(dir=self.builddir)
+            tmpfile = tempfile.mktemp(dir=self.download_path)
             with open(tmpfile, "wb") as local_file:
                 local_file.write(f.read())
-            final_path = os.path.join(self.builddir, os.path.basename(self.download['link']))
+            final_path = os.path.join(self.download_path, os.path.basename(self.download['link']))
             move(tmpfile, self.download['path'])
             return final_path
         # handle errors
@@ -43,18 +57,21 @@ class BinaryDownloader:
             pass
 
 
-    # get the html for the downloads page and parse for the download link and the timestamp
-    # based on the pages structure of
-    # <tr>
-    # <td><a href="http://downloads.mongodb.org/osx/mongodb-osx-x86_64-v2.4-latest.tgz">osx/mongodb-osx-x86_64-v2.4-latest.tgz</a></td>
-    # <td>2014-09-03 10:18:19</td>
-    # <td>87899836</td>
-    # <td><a href="http://downloads.mongodb.org/osx/mongodb-osx-x86_64-v2.4-latest.tgz.md5">md5</a></td>
-    # <td></td>
-    # <td></td>
-    # <td></td>
-    # </tr>
     def __get_latest_remote(self):
+        """
+        Populates the self.download information from the www.mogodb.org/dl site for the OS type passed into the class
+        get the html for the downloads page and parse for the download link and the timestamp
+        based on the pages structure of
+        <tr>
+            <td><a href="http://downloads.mongodb.org/osx/mongodb-osx-x86_64-v2.4-latest.tgz">osx/mongodb-osx-x86_64-v2.4-latest.tgz</a></td>
+            <td>2014-09-03 10:18:19</td>
+            <td>87899836</td>
+            <td><a href="http://downloads.mongodb.org/osx/mongodb-osx-x86_64-v2.4-latest.tgz.md5">md5</a></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        """
         d = pq(url='http://www.mongodb.org/dl/' + self.ostype + '/x86_64')
         for tr in d.items('tr'):
             a = tr.find('td').eq(0).find('a')
@@ -65,11 +82,21 @@ class BinaryDownloader:
             ts_td = tr.find('td').eq(1)
             dt = time.mktime(datetime.strptime(ts_td.text().rstrip('\n'), '%Y-%m-%d %H:%M:%S').timetuple())
             self.download = {"link": a.attr.href, "timestamp": dt,
-                             "path": os.path.join(self.builddir, os.path.basename(a.attr.href))}
+                             "path": os.path.join(self.download_path, os.path.basename(a.attr.href))}
             break
 
     def getLatest(self):
+        """
+        Gets the latest set of binaries for the OS passed on the init.
+        This will either download the load the latest set of binaries from the MongoDB.org website or it will use the
+        set of binaries that have already been downloaded.  It stores the information about the last download in a
+        checkfile that has the url, timestamp, and path to the last downloaded set of files.
 
+        If there is no checkfile or the checkfile is there but no set of binaries it will refresh downloading the
+        current latest set of binaries
+
+        :return:
+        """
         # get the checkfile file if its there
         if os.path.isfile(self.checkfile):
             # get the las_download file and unserialize it
--- util/get_binaries.py
+++ util/get_binaries.py
@@ -1,6 +1,7 @@
+import os
 from binarydownloader import BinaryDownloader
 
-downloader = BinaryDownloader('linux')
+downloader = BinaryDownloader('sunos5', os.environ.get('BUILD_DIR', os.path.dirname(os.path.realpath(__file__))))
 try:
     downloader.getLatest()
 except Exception:
