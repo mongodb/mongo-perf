4047498aed98ebb5012b9060d07495ea53ad0b23
dalyd <david.daly@mongodb.com>
2015-10-26 15:53:09 -0400

    PERF-11: Add bulk insert mongo-perf test

--- testcases/simple_insert.js
+++ testcases/simple_insert.js
@@ -74,6 +74,64 @@ tests.push( { name: "Insert.JustID",
                     doc: { _id: { "#OID": 1 } } }
               ] } );
 
+
+// variables for vector insert test
+// 100 documents per insert
+var batchSize = 100;
+var docs = []
+for (var i = 0; i < batchSize; i++) {
+    docs.push( {x: 1} )
+}
+
+/*
+ * Setup:
+ * Test: Insert a vector of documents. Each document has an integer field
+ * Notes: Generates the _id field on the client
+ *        
+ */
+tests.push( { name: "Insert.IntVector",
+              tags: ['insert','regression'],
+              pre: function( collection ) { collection.drop(); },
+              ops: [
+                  { op:  "insert",
+                    doc: docs }
+              ] } );
+
+
+// Variables for vector insert of large documents
+// 100 documents per batch
+batchSize = 100;
+// 1024 byte string in the document 
+var docSize = 1024;
+function makeDocument(docSize) {
+        var doc = { "fieldName":"" };
+        while(Object.bsonsize(doc) < docSize) {
+            doc.fieldName += "x";
+        }
+        return doc;
+    }
+}
+doc = makeDocument(docSize);
+var docs = []
+for (var i = 0; i < batchSize; i++) {
+    docs.push(doc)
+}
+/*
+ * Setup:
+ * Test: Insert a vector of large documents. Each document contains a long string
+ * Notes: Generates the _id field on the client
+ *        
+ */
+tests.push( { name: "Insert.LargeDocVector",
+              tags: ['insert','regression'],
+              pre: function( collection ) { collection.drop(); },
+              ops: [
+                  { op:  "insert",
+                    doc: docs }
+              ] } );
+
+
+
 /*
  * Setup: Create index on field 'a'
  * Test: Insert empty documents into collection using sequential int for _id
