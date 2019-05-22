e76440685e78dac8fd33d13f09b323c43dcb022c
Charlie Swanson <charlie.swanson@mongodb.com>
2019-05-22 10:14:31 -0400

    PERF-1825 Add microbenchmarks for new pipeline-style findAndModify

--- testcases/simple_commands.js
+++ testcases/simple_commands.js
@@ -133,6 +133,33 @@ tests.push( { name: "Commands.FindAndModifySortedUpdate",
                   }
               ] } );
 
+/**
+ * Semantically equivalent to FindAndModifySortedUpdate except uses a
+ * pipeline-style update to increment the count field.
+ */
+tests.push( { name: "Commands.FindAndModifySortedUpdateWithPipeline",
+              tags: ["command","regression"],
+              pre: function setUpFindAndModifySortedUpdateWithPipeline( collection ) {
+                  collection.drop();
+                  Random.setRandomSeed(22002);
+                  var nDocs = 5000;
+                  var bulk = collection.initializeUnorderedBulkOp();
+                  for (var i = 0; i < nDocs; i++) {
+                      bulk.insert({ count: 0, rand: Random.rand() });
+                  }
+                  bulk.execute();
+              },
+              ops: [
+                  { op: "command",
+                    ns: "#B_DB",
+                    command: { findAndModify: "#B_COLL",
+                               query: {},
+                               update: [{$set: {count: {$add: ["$count", 1]}}}],
+                               sort: { count: 1, rand: 1 }
+                    }
+                  }
+              ] } );
+
 /*
  * Setup: Create collection of documents with timestamps.
  * Test: Call findAndModify with a sort on the timestamp, deleting the document, then inserting a
@@ -192,6 +219,34 @@ tests.push( { name: "Commands.FindAndModifySortedUpdateIndexed",
                   }
               ] } );
 
+/**
+ * Semantically equivalent to FindAndModifySortedUpdateIndexed except uses a
+ * pipeline-style update to increment the count field.
+ */
+tests.push( { name: "Commands.FindAndModifySortedUpdateIndexedWithPipeline",
+              tags: ["command","regression"],
+              pre: function setUpFindAndModifySortedUpdate( collection ) {
+                  collection.drop();
+                  Random.setRandomSeed(22002);
+                  var nDocs = 5000;
+                  var bulk = collection.initializeUnorderedBulkOp();
+                  for (var i = 0; i < nDocs; i++) {
+                      bulk.insert({ count: 0, rand: Random.rand() });
+                  }
+                  bulk.execute();
+                  collection.ensureIndex({count: 1, rand: 1});
+              },
+              ops: [
+                  { op: "command",
+                    ns: "#B_DB",
+                    command: { findAndModify: "#B_COLL",
+                               query: {},
+                               update: [{$set: {count: {$add: ["$count", 1]}}}],
+                               sort: { count: 1, rand: 1 }
+                    }
+                  }
+              ] } );
+
 /*
  * Setup: Create collection of documents with timestamps, and create an index on the timestamp
  * field.
@@ -222,6 +277,128 @@ tests.push( { name: "Commands.FindAndModifySortedDeleteIndexed",
                   },
                   { op:  "insert",
                     doc: { ts : { "#CUR_DATE" : 0 } } }
+              ] } );
+
+function addExtraCreditPipeline() {
+    return [
+        {
+          $set: {
+              grades: {
+                  $map: {
+                      input: '$grades',
+                      in : {
+                          $cond: {
+                              if: {
+                                  $eq: [
+                                      '$$this._id',
+                                      {$concat: [
+                                          'assignment_',
+                                          {$toString: {"#RAND_INT": [0, 50]}}
+                                      ]}
+                                  ]
+                              },
+                              then: {
+                                  $mergeObjects: [
+                                      '$$this',
+                                      {
+                                        grade: {
+                                            $add: ['$$this.grade', 5],
+                                        },
+                                      },
+                                  ],
+                              },
+                              else: '$$this',
+                          },
+                      },
+                  },
+              },
+          },
+        },
+        {$set: {overall_grade: {$avg: '$grades.grade'}}},
+    ];
+};
+
+/*
+ * Setup: Create collection of documents with grades for a particular student.
+ * Test: Call findAndModify to add extra credit to an assignment and re-compute the student's total
+ * grade, returning the new total grade.
+ */
+tests.push( { name: "Commands.FindAndModifyGradeAdjustment",
+              tags: ["command","regression", "charlie"],
+              pre: function setUpFindAndModifySortedDelete( collection ) {
+                  collection.drop();
+                  Random.setRandomSeed(22002);
+                  var nDocs = 5000;
+                  var bulk = collection.initializeUnorderedBulkOp();
+                  for (var i = 0; i < nDocs; i++) {
+                      var grades = [];
+                      var total_score = 0;
+                      for (var j = 0; j < 50; j++) {
+                          score = 50 + Random.randInt(50);  // Between 50 and 100.
+                          grades.push({_id: "assignment_" + j, grade: score});
+                          total_score += score;
+                      }
+                      bulk.insert({
+                          _id: i,
+                          student_name: "placeholder name",
+                          overall_grade: total_score / 50,
+                          grades: grades
+                      });
+                  }
+                  bulk.execute();
+              },
+              ops: [
+                  { op: "command",
+                    ns: "#B_DB",
+                    command: { findAndModify: "#B_COLL",
+                               query: {_id: {"#RAND_INT": [0, 5000]}},
+                               update: addExtraCreditPipeline(),
+                               new: true,
+                               fields: {overall_grade: 1}
+                    }
+                  },
+              ] } );
+
+/*
+ * Setup: Create collection of documents with grades for a particular student.
+ * Test: Call findAndModify to add extra credit to the student with the lowest grade and re-compute
+ * the student's total grade, returning the new total grade.
+ */
+tests.push( { name: "Commands.FindAndModifyGradeAdjustment",
+              tags: ["command","regression", "charlie"],
+              pre: function setUpFindAndModifySortedDelete( collection ) {
+                  collection.drop();
+                  Random.setRandomSeed(22002);
+                  var nDocs = 5000;
+                  var bulk = collection.initializeUnorderedBulkOp();
+                  for (var i = 0; i < nDocs; i++) {
+                      var grades = [];
+                      var total_score = 0;
+                      for (var j = 0; j < 50; j++) {
+                          score = 50 + Random.randInt(50);  // Between 50 and 100.
+                          grades.push({_id: "assginment_" + j, grade: score});
+                          total_score += score;
+                      }
+                      bulk.insert({
+                          _id: i,
+                          student_name: "placeholder name",
+                          overall_grade: total_score / 50,
+                          grades: grades
+                      });
+                  }
+                  bulk.execute();
+              },
+              ops: [
+                  { op: "command",
+                    ns: "#B_DB",
+                    command: { findAndModify: "#B_COLL",
+                               query: {},
+                               update: addExtraCreditPipeline(),
+                               new: true,
+                               fields: {overall_grade: 1},
+                               sort: {overall_grade: -1}
+                    }
+                  },
               ] } );
 
 /*
