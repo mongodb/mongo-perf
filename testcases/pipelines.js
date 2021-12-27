6e5edc026d9e8c506f025f8cdff3dfb263d24b23
Mickey. J Winters <mickey.winters@mongodb.com>
2021-12-27 16:32:49 +0000

    start parameterizing

--- testcases/pipelines.js
+++ testcases/pipelines.js
@@ -737,31 +737,38 @@ for(const arrSize of [50, 250, 500]) {
     }
 }
 
+for (const groupSize of [100, 250]) {
+    const nDocs = 10 * 1000;
+    for (const n of [10, 50]) {
 /**
  * Test cases similar to the $minN/$maxN window function tests but for $top/$bottom/$topN/$bottomN.
  * This test calculates a rolling leader-board/loser-board so you can see who is the best and worst
  * golf players over time. The window is over the last 20 games and it picks the top or bottom
  * 20 players for each window with $topN/$bottomN, or single best/worst player of the window for
- * $top/$bottomN. There are 3 divisions each with 20 players.
+ * $top/$bottomN. Each division has n * 2 players.
  */
 [
     {name: "Top", op: "$top"},
     {name: "Bottom", op: "$bottom"},
-    {name: "TopN", op: "$topN", n: 5},
-    {name: "BottomN", op: "$bottomN", n: 5}
+    {name: "TopN", op: "$topN", n},
+    {name: "BottomN", op: "$bottomN", n}
 ].forEach(({name, op, n}) => {
+    const players = n * 2;
+    const divisions = parseInt(nDocs / groupSize);
     generateTestCase({
-        name: "SetWindowFields.ThreePartitionsWith" + name,
+        name: "SetWindowFields.PartitionsSizeOf" + groupSize + "With" + name + (n ? "AndN" + n : ""),
         tags: ['>=5.2.0'],
-        nDocs: 1000,
+        nDocs,
         docGenerator: (i) => ({
             _id: i,
             // The window function version always inserts so we don't need a specific direction like
             // in the group tests. For testing purposes players always get worse with time to
             // maximize comparisons.
-            score: parseInt(i / 3),
-            division: i % 3,
-            playerId: i % 60
+            score: i,
+            // Dividing i by players gives n * 2 players to each division at a time. If we didn't do this
+            // division1 would have all of player 1's games  and division2 would have all of player 2's games, etc...
+            division: parseInt(i / players) % nDivisions,
+            playerId: i % (players * divisions)
         }),
         pipeline: [{$setWindowFields: {
             sortBy: {_id: 1},
