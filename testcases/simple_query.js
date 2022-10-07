50877158d6026da2d1f1ac785113cf703264c42e
Zixuan Zhuang <zixuan.zhuang@mongodb.com>
2022-10-07 15:37:54 -0700

    address comments

--- testcases/simple_query.js
+++ testcases/simple_query.js
@@ -1116,13 +1116,6 @@ if (typeof(tests) !== "object") {
         query: {"a": {$gt: 1}, "b": {$lt: 900}}
     });
     addTestCaseWithLargeDatasetAndIndexes({
-        name: "RangeQuery_MultipleIndexes_LowSelectivityMatch_LS", 
-        tags: ["indexed"],
-        docGenerator: smallDoc,
-        indexes: [{"a":1}, {"b":1}, {"a":1, "b":1}],
-        query: {"a": {$gt: 1}, "b": {$lt: 900}}
-    });
-    addTestCaseWithLargeDatasetAndIndexes({
         name: "PointQuerySubField_SingleIndex_LL", 
         docGenerator: largeDoc,
         indexes: [{"e.a":1}],
@@ -1137,7 +1130,7 @@ if (typeof(tests) !== "object") {
 
     // Select ~1% from a single indexed field.
     addTestCaseWithLargeDatasetAndIndexes({
-        name: "RangeQuery_SingleIndex_SimpleRange_LowSelectivityMatch_LS",
+        name: "RangeQuery_SingleIndex_SimpleRange_LS",
         tags: ["indexed"],
         docGenerator: smallDoc,
         indexes: [{"b": 1}],
@@ -1146,14 +1139,14 @@ if (typeof(tests) !== "object") {
 
     // Select ~99% from a single indexed field.
     addTestCaseWithLargeDatasetAndIndexes({
-        name: "RangeQuery_SingleIndex_SimpleRange_LS",
+        name: "RangeQuery_SingleIndex_SimpleRange_LowSelectivityMatch_LS",
         tags: ["indexed"],
         docGenerator: smallDoc,
         indexes: [{"b": 1}],
         query: {"b": {$gt: 1, $lt: 999}}
     });
 
-    // Select ~90% from two indexed field.
+    // Select ~90% with two range predicates on two indexed fields of a compound index.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_MultiFields_SingleIntervals_LS",
         tags: ["indexed"],
@@ -1171,7 +1164,8 @@ if (typeof(tests) !== "object") {
         query: {$or: [{"b": {$gt: 99}}, {"b": {$lt: 9}}, {"b": {"$in": largeArrayRandom}}]}
     });
 
-    // Select ~99% from two indexed field.
+    // Select ~99% from two indexed fields of a compound index. There is a range predicate on the
+    // leading field and a union of point predicates on the trailing field.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_GenericPlan_TwoFields_LS",
         tags: ["indexed"],
@@ -1180,7 +1174,8 @@ if (typeof(tests) !== "object") {
         query: {"h": {$gt: 1}, "b": {"$in": largeArrayRandom}}
     });
 
-    // Select ~99% from three indexed field.
+    // Select ~99% from three indexed fields of a compound index. There is a range predicate on 
+    // the leading field and unions of point intervals on the trailing fields.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_GenericPlan_ThreeFields_LS",
         tags: ["indexed"],
@@ -1189,7 +1184,7 @@ if (typeof(tests) !== "object") {
         query: {"h": {$gt: 1}, "b": {"$in": largeArrayRandom}, "a": {"$in": largeArrayRandom}}
     });
 
-    // Select ~99% from two indexed field.
+    // Select ~99% from two indexed fields of a compound index with range predicates on both fields.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_GenericPlan_TwoFields_Range_LS",
         tags: ["indexed"],
@@ -1198,7 +1193,8 @@ if (typeof(tests) !== "object") {
         query: {"h": {$gt: 1}, "b": {$lt: 100}}
     });
 
-    // Select ~99% from three indexed field.
+    // Select ~99% from three indexed fields of a compound index with range predicates on all
+    // three fields.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_GenericPlan_ThreeFields_Range_LS",
         tags: ["indexed"],
@@ -1207,7 +1203,7 @@ if (typeof(tests) !== "object") {
         query: {"h": {$gt: 1}, "b": {$lt: 100}, "a": {$gt: 1}}
     });
 
-    // Select ~99% from five indexed field.
+    // Select ~99% from five indexed fields of a compound index.
     addTestCaseWithLargeDatasetAndIndexes({
         name: "RangeQuery_SingleIndex_GenericPlan_FiveFields_Range_LS",
         tags: ["indexed"],
