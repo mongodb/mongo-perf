fbc29ee5b451ca73268a9207d55e47f74af74eb9
Alberto Massari <alberto.massari@mongodb.com>
2022-11-03 15:15:41 +0100

    PERF-3484 Benchmark update operation not requiring index updates (#111)

--- testcases/long_multi_update.js
+++ testcases/long_multi_update.js
@@ -68,10 +68,10 @@ var testBigAllDocsMultiChange = [
 tests.push( { name: "MultiUpdate.BigAllDocsMultiChange.NoIndex",
               tags: ['update','slow'],
               pre: function( collection ) {
-                  setupTestBigAllDocsMultiChange( collection );                                
+                  setupTestBigAllDocsMultiChange( collection );
               },
               ops: testBigAllDocsMultiChange,
-            } );       
+            } );
               
 /*
  * Setup: Create a collection with 100k documents with integer _id,
@@ -84,11 +84,31 @@ tests.push( { name: "MultiUpdate.BigAllDocsMultiChange.Indexed",
               tags: ['update','slow','indexed'],
               pre: function( collection ) {
                   setupTestBigAllDocsMultiChange( collection );
-                  collection.createIndex( { x : 1 } );                                    
-                  collection.createIndex( { y : 1 } );                                    
+                  collection.createIndex( { x : 1 } );
+                  collection.createIndex( { y : 1 } );
               },
               ops: testBigAllDocsMultiChange,
-            } );               
+            } );
+
+/*
+ * Setup: Create a collection with 100k documents with integer _id,
+ *        and integer field x = 0, y='b', both indexed
+ * Test: Increment the x field on all 100k documents. Test updates
+ *       only one index.
+ */
+tests.push( { name: "MultiUpdate.BigAllDocsMultiChangeIgnoredIndex.Indexed",
+              tags: ['update','slow','indexed'],
+              pre: function( collection ) {
+                  setupTestBigAllDocsMultiChange( collection );
+                  collection.createIndex( { x : 1 } );
+                  collection.createIndex( { y : 1 } );
+              },
+              ops: [
+                  { op:  "update",
+                    multi: true,
+                    query: { },
+                    update: { $inc: { x : 1 } } },
+              ] } );
 
 var setupTestContendedAllDocs = function( collection ) {
    collection.drop();
