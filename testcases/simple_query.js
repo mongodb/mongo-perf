910c5550dd01bed00888b2e73d020cdec5a78049
Mihai Andrei <mihai.andrei@10gen.com>
2022-11-23 15:28:25 -0500

    try reverting large collections change

--- testcases/simple_query.js
+++ testcases/simple_query.js
@@ -495,6 +495,45 @@ if (typeof(tests) !== "object") {
         op: {op: "find", query: {}}
     });
 
+    /**
+     * Similar to 'addTestCase' but sets up the test to be able to share collections if running
+     * as part of a suite that opts-in for sharing.
+     * @param {query} - The query to benchmark with 'find' operation. Ignored if 'op' is defined.
+     * @param {op} - The full definition of the op to be benchmarked.
+     * @param {Number} [options.nDocs = largeCollectionSize] - The number of documents to insert in
+     * the collection. Ignored, if 'generateData' is defined.
+     * @param {function} [options.docGenerator] - To be used with populatorGenerator. Ignored, if
+     * 'generatedData' is defined.
+     * @param {function} [options.generateData = populatorGenerator] - Uses 'docGenerator' to populate
+     * the collection with 'nDocs' documents. If the test is part of a suite that uses '--shareDataset'
+     * flag, the generator is run once (for the first test in the suite).
+     * @param {function} [options.pre=noop] - Any other setup, in addition to creating the data, that
+     * the test might need. For example, creating indexes. The 'pre' fixture is run per test, so for
+     * tests that share the dataset, the effects must be undone with 'post'.
+     * @param {function} [options.post=noop] - cleanup after the test is done.
+     */
+    function addTestCaseWithLargeDataset(options) {
+        const largeCollectionSize = 100000;
+        var nDocs = options.nDocs || largeCollectionSize;
+        var tags = options.tags || [];
+        tests.push({
+            tags: ["regression", "query_large_dataset"].concat(tags),
+            name: "Queries." + options.name,
+            generateData: options.generateData ||
+                function(collection) {
+                    Random.setRandomSeed(258);
+                    collection.drop();
+                    var bulkop = collection.initializeUnorderedBulkOp();
+                    for (var i = 0; i < nDocs; i++) {
+                        bulkop.insert(options.docGenerator(i));
+                    }
+                    bulkop.execute();
+                },
+            pre: options.pre || function (collection) {},
+            post: options.post || function(collection) {},
+            ops: ("op" in options) ? [options.op] : [{op: "find", query: options.query}],
+        });
+    }
 
     /**
      * Benchmarks for find on large collections, targeting the basic functionality of the engine in
@@ -660,5 +699,156 @@ if (typeof(tests) !== "object") {
         name: "ProjectWithArithExpressions_CollScan_LS",
         docGenerator: smallDoc,
         op: {op: "find", query: {}, filter: projectWithArithExpressions}
+    });
+
+    // Tests: indexed plans
+    let dropIndexesAndCaches = function (collection) {
+        collection.dropIndexes();
+        collection.getPlanCache().clear();
+    }
+    let createIndexes = function (collection, indexes) {
+        indexes.forEach(function (index) {
+            assert.commandWorked(collection.createIndex(index));
+        });
+    }
+
+    function addTestCaseWithLargeDatasetAndIndexes(options) {
+        options.pre = function (collection) {
+            dropIndexesAndCaches(collection);
+            createIndexes(collection, options.indexes);
+        };
+        options.post = dropIndexesAndCaches;
+        addTestCaseWithLargeDataset(options);
+    }
+
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "PointQuery_SingleIndex_LL",
+        docGenerator: largeDoc,
+        indexes: [{"a": 1}],
+        query: {"a": 7}
+    });
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_SingleIndex_LowSelectivityMatch_LL",
+        docGenerator: largeDoc,
+        indexes: [{"a": 1}],
+        query: {"a": {$gt: 1}}
+    });
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "PointQuery_MultipleIndexes_LL",
+        docGenerator: largeDoc,
+        indexes: [{"a": 1}, {"b": 1}, {"a": 1, "b": 1}],
+        query: {"a": 7, "b": 742}
+    });
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_MultipleIndexes_LowSelectivityMatch_LL",
+        tags: ["indexed"],
+        docGenerator: largeDoc,
+        indexes: [{"a": 1}, {"b": 1}, {"a": 1, "b": 1}],
+        query: {"a": {$gt: 1}, "b": {$lt: 900}}
+    });
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "PointQuerySubField_SingleIndex_LL",
+        docGenerator: largeDoc,
+        indexes: [{"e.a": 1}],
+        query: {"e.a": 7}
+    });
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuerySubField_SingleIndex_LowSelectivityMatch_LL",
+        docGenerator: largeDoc,
+        indexes: [{"e.a": 1}],
+        query: {"e.a": {$gt: 1}}
+    });
+
+    // Select ~1% from a single indexed field.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_SingleIndex_SimpleRange_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"b": 1}],
+        query: {"b": {$gt: 100, $lt: 109}}
+    });
+
+    // Select ~99% from a single indexed field.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_SingleIndex_SimpleRange_LowSelectivityMatch_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"b": 1}],
+        query: {"b": {$gt: 1, $lt: 999}}
+    });
+
+    /**
+     * Large arrays used for $in queries in the subsequent test cases.
+     */
+    var nLargeArrayElements = 1000;
+    var largeArrayRandom = [];
+    for (var i = 0; i < nLargeArrayElements; i++) {
+        largeArrayRandom.push(Random.randInt(nLargeArrayElements));
+    }
+
+    // Select ~90% with two range predicates on two indexed fields of a compound index.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_SingleIntervals_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"b": 1, "h": 1}],
+        query: {"b": {"$in": largeArrayRandom}, "h": {$gt: 100}}
+    });
+
+    // Select ~99% from a single indexed field.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_SingleFieldIndex_ComplexBounds_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"b": 1}],
+        query: {$or: [{"b": {$gt: 99}}, {"b": {$lt: 9}}, {"b": {"$in": largeArrayRandom}}]}
+    });
+
+    // Select ~99% from two indexed fields of a compound index. There is a range predicate on the
+    // leading field and a union of point predicates on the trailing field.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_ComplexBounds_TwoFields_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"h": 1, "b": 1}],
+        query: {"h": {$gt: 1}, "b": {"$in": largeArrayRandom}}
+    });
+
+    // Select ~99% from three indexed fields of a compound index. There is a range predicate on
+    // the leading field and unions of point intervals on the trailing fields.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_ComplexBounds_ThreeFields_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"h": 1, "b": 1, "a": 1}],
+        query: {"h": {$gt: 1}, "b": {"$in": largeArrayRandom}, "a": {"$in": largeArrayRandom}}
+    });
+
+    // Select ~99% from two indexed fields of a compound index with range predicates on both fields.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_ComplexBounds_TwoFields_Range_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"h": 1, "b": 1}],
+        query: {"h": {$gt: 1}, "b": {$lt: 100}}
+    });
+
+    // Select ~99% from three indexed fields of a compound index with range predicates on all
+    // three fields.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_ComplexBounds_ThreeFields_Range_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"h": 1, "b": 1, "a": 1}],
+        query: {"h": {$gt: 1}, "b": {$lt: 100}, "a": {$gt: 1}}
+    });
+
+    // Select ~99% from five indexed fields of a compound index.
+    addTestCaseWithLargeDatasetAndIndexes({
+        name: "RangeQuery_CompoundIndex_ComplexBounds_FiveFields_Range_LS",
+        tags: ["indexed"],
+        docGenerator: smallDoc,
+        indexes: [{"h": 1, "b": 1, "e.b": 1, "d": 1, "e.h": 1}],
+        query: {"h": {$gt: 1}, "b": {$lt: 100}, "e.b": {$gt: 1}, "d": {$gt: 10}, "e.h": {$gt: 1}}
     });
 }());
