03d4feb64d600d996cfa2394c18dfaffcca1fd15
Kevin Cherkauer <111792207+kevin-cherkauer@users.noreply.github.com>
2023-12-13 08:51:31 -0800

    PERF-4977 Add MinAccTopField_LLR10 microbench variant w realistic fields (#144)
    * PERF-4977 Add MinAccTopField_LLR10 microbench variant w realistic fields
    
    * perf4977 p02 Add missing $ to date_last_modified in query

--- util/docGenerators.js
+++ util/docGenerators.js
@@ -65,7 +65,6 @@ const largeDoc = function (i) {
             h: Random.rand() * 1000,
             i: Random.rand() * 100000,
         },
-
         
         f: [Random.randInt(10), Random.randInt(10), Random.randInt(10)],
         g: Random.rand() * 10,
@@ -97,5 +96,63 @@ const largeDoc = function (i) {
         gg: Random.rand() * 10,
         hh: Random.rand() * 1000,
         ii: Random.rand() * 100000,
+    };
+}
+
+/**
+ * Variant of largeDoc() that has field name lengths and sometimes shared field name prefixes more
+ * typical of a production database. The doc contents are otherwise the same.
+ *
+ * @param {Number} i - the number to be used as _id
+ * @returns - a document of size ~8500 bytes (Object.bsonsize(largeDoc(N)))
+ */
+const largeDocLargeFieldNames = function (i) {
+    return {
+        _id: i,
+        customer: Random.randInt(10),
+        tracking_number: Random.randInt(1000),
+        date_created: Random.rand() * 100 + 1, // no zeros in this field
+        date_last_modified: i % 10000,
+        order_details: {
+            antelopes: Random.randInt(10),
+            brontosauri: Random.randInt(1000),
+            price: Random.rand() * 100 + 1,
+            widgets: { u: Random.randInt(100), v: Random.randInt(100) },
+            special_widgets: [Random.randInt(10), Random.randInt(10), Random.randInt(10)],
+            sale_price: Random.rand() * 10,
+            discount_code: Random.rand() * 1000,
+            promotion_code: Random.rand() * 100000,
+        },
+
+        order_ids: [Random.randInt(10), Random.randInt(10), Random.randInt(10)],
+        timestamp: Random.rand() * 10,
+        credit_card_number: Random.rand() * 1000,
+        phone_number: Random.rand() * 100000,
+
+        // Fields the queries won't be accessing but might need to copy/scan over.
+        customer_comments: [quotes, quotes, quotes, quotes, quotes],
+        favorite_products: { author: " Jane Austen", work: "Emma", quotes: quotes },
+        frequently_viewed_pages: { a: quotes[0] + i.toString(), b: quotes[2] + (i % 10).toString(), c: quotes[4] },
+        browsing_history: [quotes, quotes, quotes, quotes, quotes],
+
+        // Fields towards the end of the object some of the tests will be using.
+        order_month: Random.randInt(10),
+        browsing_time: Random.randInt(1000),
+        quantity: Random.rand() * 100 + 1,
+        returned_date: i % 10000,
+        tracking_details: {
+            carrier: Random.randInt(10),
+            carrier_phone: Random.randInt(1000),
+            shipping_price: Random.rand() * 100 + 1,
+            fulfillment_center: { u: Random.randInt(100), v: Random.randInt(100) },
+            location_history: [Random.randInt(10), Random.randInt(10), Random.randInt(10)],
+            back_order_date: Random.rand() * 10,
+            weight_kg: Random.rand() * 1000,
+            oversized: Random.rand() * 100000,
+        },
+        geography: [Random.randInt(10), Random.randInt(10), Random.randInt(10)],
+        ship_date: Random.rand() * 10,
+        shipping_speed: Random.rand() * 1000,
+        delivery_datetime: Random.rand() * 100000,
     };
 }
